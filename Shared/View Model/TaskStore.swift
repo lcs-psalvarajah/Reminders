0678ae7de0bdc7d2a8d326699d976c76033a8928
jina <psalvarajah@lcs.on.ca>
2022-01-25 09:17:51 -0500

    Added swipe to delete functionality on iOS

--- Shared/View Model/TaskStore.swift
+++ Shared/View Model/TaskStore.swift
@@ -8,10 +8,20 @@
 import Foundation
 
 class TaskStore: ObservableObject {
+    // MARK: Stored properties
     @Published var tasks: [Task]
     
+    // MARK: Initializers
     init(tasks: [Task] = []) {
         self.tasks = tasks
+    }
+    
+    // MARK: Functions
+    func deleteItems(at offsets: IndexSet) {
+        // "offsets" contain a set of items elescted for deletion
+        // The set is then passed to the built-in "remove" method on
+        // the "tasks" array which handles removal from the array
+        tasks.remove(atOffsets: offsets)
     }
 }
 
